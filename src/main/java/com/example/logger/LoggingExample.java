5011f894075357b9d9d311452a96f06eac2ad439
unknown <daehwan.lee>
2024-08-02 13:02:18 +0900

    added \Logging Example

---
+++ src/main/java/com/example/logger/LoggingExample.java
@@ -0,0 +1,21 @@
+package com.example.logger;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LoggingExample {
+    private static final Logger logger = LoggerFactory.getLogger(LoggingExample.class);
+
+    public static void main(String[] args) {
+        logger.debug("This is a debug message");
+        logger.info("This is an info message");
+        logger.warn("This is a warn message");
+        logger.error("This is an error message");
+
+        try {
+            throw new Exception("Test exception");
+        } catch (Exception e) {
+            logger.error("Exception caught", e);
+        }
+    }
+}
